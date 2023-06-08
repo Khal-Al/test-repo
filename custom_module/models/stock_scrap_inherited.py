adc8b3b82d5324af032b6328233aab7bd949e51d
Khal-Al <135505241+Khal-Al@users.noreply.github.com>
2023-06-08 16:15:21 +0300

    Update stock_scrap_inherited.py

--- custom_module/models/stock_scrap_inherited.py
+++ custom_module/models/stock_scrap_inherited.py
@@ -4,5 +4,5 @@ from odoo import fields, models
 class StockScrapInherited(models.Model):
     _inherit = 'stock.scrap'
 
-    origin = fields.Char(string="Source of Failure")
+    origin = fields.Char(string="Reason of Failure")
 
